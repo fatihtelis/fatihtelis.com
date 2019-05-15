591651ff6295a6bcafa7095b230b2eecc96cec26
Fatih Telis <fatih.telis@fabrikamedya.com.tr>
2019-05-15 17:28:31 +0300

    Deploy to gh-pages

---
+++ out/_next/static/webpack/static/development/pages/index.js.3972df3353e7057dca23.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/screens/about/about.json":
+/*!****************************************!*\
+  !*** ./pages/screens/about/about.json ***!
+  \****************************************/
+/*! exports provided: 0, 1, 2, 3, 4, 5, 6, 7, default */
+/***/ (function(module) {
+
+module.exports = [{"icon":"bx-user-plus","text":"I was born in 1985 as an ordinary child."},{"icon":"bx-book-bookmark","text":"I've been graduated as a chemical engineer from Boğaziçi University (İstanbul) in 2009."},{"icon":"bx-wrench","text":"I had worked as an R&D enginner in a tire manufacturing company for some time."},{"icon":"bx-directions","text":"After realizing chemical engineering is not my dream job, I've changed the way of my career and started what I like to do, “coding”."},{"icon":"bx-code","text":"I have attended a 6-month web development course in Bilge Adam and got special courses about web development."},{"icon":"bx-code","text":"I have attended a 6-month web development course in Bilge Adam and started my first personal project, YouFolders.com."},{"icon":"bx-briefcase","text":"I have been working as a Frontend Developer as a professional since 2018."},{"icon":"bxl-react","text":"I'm using React (with Next.js) in my projects as my main JS Framework."}];
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.3972df3353e7057dca23.hot-update.js.map
