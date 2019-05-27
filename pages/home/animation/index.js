59bbfa0e41554c7cc35d9a6783e90913fd202ac2
Fatih Telis <fatih.telis@gmail.com>
2019-05-28 00:45:37 +0300

    Animation class updated to function with hooks

--- pages/home/animation/index.js
+++ pages/home/animation/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import * as THREE from 'three';
 // import OrbitControls from 'three-orbitcontrols';
@@ -13,7 +14,7 @@ const Outer = styled.div`
 
 const colors = {
   cube: new THREE.Color(0xf6c523),
-  sphere: new THREE.Color(0x1b00ff),
+  sphere: new THREE.Color(0x10027a),
   prism: new THREE.Color(0xc82121),
   sky: new THREE.Color(0x000000),
   ambient: new THREE.Color(0xffffff),
@@ -25,55 +26,122 @@ const colors = {
   background: new THREE.Color(0xf5f7fa),
 };
 
-class Animation extends React.Component {
-  state = {
-    activeScreen: this.props.activeScreen,
+const Animation = ({ activeScreen }) => {
+  const active = useRef(activeScreen);
+  const canvas = useRef(null);
+  let cube;
+  let sphere;
+  let prism;
+  let camera;
+  let renderer;
+  let scene;
+
+  const renderCanvas = () => {
+    requestAnimationFrame(renderCanvas);
+
+    cube.rotation.y += 0.01;
+    sphere.rotation.y += 0.01;
+    prism.rotation.y += 0.01;
+    switch (active.current) {
+      case 'about':
+        if (cube.position.z < 21) {
+          cube.position.x += 0.36;
+          cube.position.z += 0.75;
+          cube.rotation.x -= 0.15;
+          cube.rotation.z += 0.1;
+        } else {
+          cube.rotation.x = 0;
+          cube.rotation.y = 0;
+          cube.rotation.z = 0;
+        }
+        break;
+      case 'portfolio':
+        if (sphere.position.z < 21) {
+          sphere.position.y -= 0.05;
+          sphere.position.z += 0.75;
+          sphere.rotation.x -= 0.15;
+          sphere.rotation.z += 0.1;
+        } else {
+          sphere.rotation.x = 0;
+          sphere.rotation.y = 0;
+          sphere.rotation.z = 0;
+        }
+        break;
+      case 'contact':
+        if (prism.position.z < 21) {
+          prism.position.x -= 0.35;
+          prism.position.y -= 0.04;
+          prism.position.z += 0.75;
+          prism.rotation.x -= 0.05;
+        } else {
+          prism.rotation.x += 0;
+          prism.rotation.y = 0;
+        }
+        break;
+      case 'cv':
+        if (camera.rotation.x < Math.PI / 2) {
+          camera.rotation.x += 0.05;
+        }
+        break;
+      default:
+        if (cube.position.z > 0) {
+          cube.position.x -= 0.48;
+          cube.position.z -= 1;
+        }
+        if (sphere.position.z > 0) {
+          sphere.position.y += 0.06666666;
+          sphere.position.z -= 1;
+        }
+        if (prism.position.z > 0) {
+          prism.position.x += 0.46666666;
+          prism.position.y += 0.05333333;
+          prism.position.z -= 1;
+          prism.rotation.x += 0.06666666;
+        }
+        if (camera.rotation.x > 0) {
+          camera.rotation.x -= 0.1;
+        }
+        break;
+    }
+
+    renderer.render(scene, camera);
   };
 
-  async componentDidMount() {
-    this.startAnimation();
-  }
-
-  componentWillReceiveProps({ activeScreen }) {
-    if (this.props.activeScreen !== activeScreen) {
-      this.setState({ activeScreen });
-    }
-  }
-
-  startAnimation = () => {
+  const startAnimation = () => {
     // Initializing Three.js Scene
-    const scene = new THREE.Scene();
+    scene = new THREE.Scene();
 
     // Setting Camera
     const dislayRatio = window.innerWidth / window.innerHeight;
-    const camera = new THREE.PerspectiveCamera(75, dislayRatio, 0.1, 50);
+    camera = new THREE.PerspectiveCamera(75, dislayRatio, 0.1, 50);
     camera.position.y = 6;
     camera.position.z = 24;
 
     // Setting Renderer
-    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.shadowMap.enabled = true;
     renderer.gammaInput = true;
     renderer.gammaOutput = true;
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
+
     // Set background color
     renderer.setClearColor(colors.background, 1);
 
     // Adding ,renderer to Canvas
-    this.canvas.appendChild(renderer.domElement);
+    canvas.current.appendChild(renderer.domElement);
 
     // Ambient Light
     const ambient = new THREE.AmbientLight(colors.ambient, 0.75);
     scene.add(ambient);
 
     // Directional Light - Left
-    const directLightLeft = new THREE.DirectionalLight(colors.directional.left, 0.5);
+    const directLightLeft = new THREE.DirectionalLight(colors.directional.left, 1);
     directLightLeft.position.set(-20, 40, 0);
     scene.add(directLightLeft);
 
     // Directional Light - Right
-    const directLightRight = new THREE.DirectionalLight(colors.directional.right, 0.5);
+    const directLightRight = new THREE.DirectionalLight(colors.directional.right, 1);
     directLightRight.position.set(20, 40, 0);
     scene.add(directLightRight);
 
@@ -108,7 +176,7 @@ class Animation extends React.Component {
       color: colors.cube,
     });
 
-    const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
     cube.position.x = -10;
     cube.position.y = 6.5;
     cube.rotation.y = Math.PI / 4;
@@ -121,7 +189,7 @@ class Animation extends React.Component {
       color: colors.sphere,
     });
 
-    const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
     sphere.position.y = 6.5;
     sphere.castShadow = true;
     scene.add(sphere);
@@ -132,7 +200,7 @@ class Animation extends React.Component {
       color: colors.prism,
     });
 
-    const prism = new THREE.Mesh(prismGeometry, prismMaterial);
+    prism = new THREE.Mesh(prismGeometry, prismMaterial);
     prism.position.x = 10;
     prism.position.y = 6.5;
     prism.castShadow = true;
@@ -140,79 +208,6 @@ class Animation extends React.Component {
 
     // Orbit Controls
     // const controls = new OrbitControls(camera, this.canvas);
-
-    const renderCanvas = async () => {
-      const { activeScreen } = this.state;
-      requestAnimationFrame(renderCanvas);
-
-      cube.rotation.y += 0.01;
-      sphere.rotation.y += 0.01;
-      prism.rotation.y += 0.01;
-
-      switch (activeScreen) {
-        case 'about':
-          if (cube.position.z < 21) {
-            cube.position.x += 0.36;
-            cube.position.z += 0.75;
-            cube.rotation.x -= 0.15;
-            cube.rotation.z += 0.1;
-          } else {
-            cube.rotation.x = 0;
-            cube.rotation.y = 0;
-            cube.rotation.z = 0;
-          }
-          break;
-        case 'portfolio':
-          if (sphere.position.z < 21) {
-            sphere.position.y -= 0.05;
-            sphere.position.z += 0.75;
-            sphere.rotation.x -= 0.15;
-            sphere.rotation.z += 0.1;
-          } else {
-            sphere.rotation.x = 0;
-            sphere.rotation.y = 0;
-            sphere.rotation.z = 0;
-          }
-          break;
-        case 'contact':
-          if (prism.position.z < 21) {
-            prism.position.x -= 0.35;
-            prism.position.y -= 0.04;
-            prism.position.z += 0.75;
-            prism.rotation.x -= 0.05;
-          } else {
-            prism.rotation.x += 0;
-            prism.rotation.y = 0;
-          }
-          break;
-        case 'cv':
-          if (camera.rotation.x < Math.PI / 2) {
-            camera.rotation.x += 0.05;
-          }
-          break;
-        default:
-          if (cube.position.z > 0) {
-            cube.position.x -= 0.48;
-            cube.position.z -= 1;
-          }
-          if (sphere.position.z > 0) {
-            sphere.position.y += 0.06666666;
-            sphere.position.z -= 1;
-          }
-          if (prism.position.z > 0) {
-            prism.position.x += 0.46666666;
-            prism.position.y += 0.05333333;
-            prism.position.z -= 1;
-            prism.rotation.x += 0.06666666;
-          }
-          if (camera.rotation.x > 0) {
-            camera.rotation.x -= 0.1;
-          }
-          break;
-      }
-
-      renderer.render(scene, camera);
-    };
 
     renderCanvas();
 
@@ -228,15 +223,15 @@ class Animation extends React.Component {
     );
   };
 
-  render() {
-    return (
-      <Outer
-        ref={(n) => {
-          this.canvas = n;
-        }}
-      />
-    );
-  }
-}
+  useEffect(() => {
+    startAnimation();
+  }, []);
+
+  useEffect(() => {
+    active.current = activeScreen;
+  }, [activeScreen]);
+
+  return <Outer ref={canvas} />;
+};
 
 export default Animation;
