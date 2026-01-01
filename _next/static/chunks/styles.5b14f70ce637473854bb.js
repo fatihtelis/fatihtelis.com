75119261ab4b3b6c3ae110a59656ac486a4a7262
fatihtelis <fatihtelis@users.noreply.github.com>
2026-01-01 06:52:00 +0000

    deploy: 312e10417ae42ac21fcda39d52205446339fa43b

--- _next/static/chunks/styles.5b14f70ce637473854bb.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[11],{"1h03":function(n,o,c){},"4UDT":function(n,o,c){},MJq7:function(n,o,c){},n4JH:function(n,o,c){}}]);
--- service-worker.js
+++ service-worker.js
@@ -1,57 +1,59 @@
 self.__precacheManifest = [
   {
-    "url": "/_next/static/OEv_P7u0Y1O07eSVIoj32/_buildManifest.js",
-    "revision": "ece81c4eb12ca158dc339f775a7a652a"
+    "url": "/_next/static/chunks/5ce6bf4a.4237541b6e42e5a82604.js",
+    "revision": "cf48fa87dab994cc5099"
   },
   {
-    "url": "/_next/static/OEv_P7u0Y1O07eSVIoj32/_ssgManifest.js",
-    "revision": "abee47769bf307639ace4945f9cfd4ff"
+    "url": "/_next/static/chunks/5fcd5e32.6d33924e495300e79d86.js",
+    "revision": "bb31e235d7b095da7925"
   },
   {
-    "url": "/_next/static/chunks/5ce6bf4a.e06e4ba45c7c802ad770.js",
-    "revision": "4754ade61348b8fa6082"
+    "url": "/_next/static/chunks/805dcaae0d3e2df0646e4f8f25293146dde7df27.57812dcf43d89fa4c7b0.js",
+    "revision": "51b63438d6f1bb21b726"
   },
   {
-    "url": "/_next/static/chunks/5fcd5e32.309239c625b32e1bd39d.js",
-    "revision": "17b0e156205512e4fe67"
+    "url": "/_next/static/chunks/805dcaae0d3e2df0646e4f8f25293146dde7df27_CSS.8f6f5b95f5e92b2c9bcd.js",
+    "revision": "789c3ad5c8956bc0f34d"
   },
   {
-    "url": "/_next/static/chunks/f6078781a05fe1bcb0902d23dbbb2662c8d200b3.00aaea69a4bfc61676f6.js",
-    "revision": "6b0fcb00c156b8ac2953"
+    "url": "/_next/static/chunks/f6078781a05fe1bcb0902d23dbbb2662c8d200b3.de34e989f34eb5287adf.js",
+    "revision": "3eaab0fe8b706a87bf46"
   },
   {
-    "url": "/_next/static/chunks/fb7d5399.5ffdb6fc56ea720f444d.js",
-    "revision": "7638023588dddd5d1d53"
+    "url": "/_next/static/chunks/fb7d5399.e71126186752ab600555.js",
+    "revision": "6b0a492e8dbd866f173f"
   },
   {
-    "url": "/_next/static/chunks/framework.e22c2351f7fb91ccdf78.js",
-    "revision": "cfd6f7276ac65925c24c"
+    "url": "/_next/static/chunks/framework.baa41d4dbf5d52db897c.js",
+    "revision": "a4aee4505f0b605ffdad"
   },
   {
-    "url": "/_next/static/chunks/main-5184c23f0683e525903a.js"
+    "url": "/_next/static/chunks/main-0ee7220f23583a7d9e5d.js"
   },
   {
-    "url": "/_next/static/chunks/pages/_app-e614be418a1179cf075f.js"
+    "url": "/_next/static/chunks/pages/_app-151e16e4318da63a9ae0.js"
   },
   {
-    "url": "/_next/static/chunks/pages/_error-b52f8b678ce4b85a362a.js"
+    "url": "/_next/static/chunks/pages/_error-cb6685cdcea506c8b6dd.js"
   },
   {
-    "url": "/_next/static/chunks/pages/index-18afe69ae4f0c811ae3f.js"
+    "url": "/_next/static/chunks/pages/games-afea8210e8e20381ba9a.js"
   },
   {
-    "url": "/_next/static/chunks/polyfills-a98cee78eb8282e29fb6.js"
+    "url": "/_next/static/chunks/pages/games/privacy-04dca89a96fc1101cf5d.js"
   },
   {
-    "url": "/_next/static/chunks/styles.5b14f70ce637473854bb.js",
-    "revision": "7f151f55e15694a31353"
+    "url": "/_next/static/chunks/pages/index-e09721c372bbca77090c.js"
+  },
+  {
+    "url": "/_next/static/chunks/polyfills-555defa4e62ba07d4446.js"
   },
   {
     "url": "/_next/static/chunks/webpack-d7b2fb72fb7257504a38.js"
   },
   {
-    "url": "/_next/static/css/styles.05d9ece0.chunk.css",
-    "revision": "7f151f55e15694a31353"
+    "url": "/_next/static/css/805dcaae0d3e2df0646e4f8f25293146dde7df27_CSS.05d9ece0.chunk.css",
+    "revision": "789c3ad5c8956bc0f34d"
   },
   {
     "url": "/_next/static/fonts/boxicons-023b7b202fa7aa03b0bf6677017f560f.eot"
@@ -91,6 +93,14 @@ self.__precacheManifest = [
   },
   {
     "url": "/_next/static/fonts/sofia-pro-ultra-light-4bdc72d871a96c7a9bf467f5d5cff5f0.otf"
+  },
+  {
+    "url": "/_next/static/yaALgm2ev7-AeUuP2juzz/_buildManifest.js",
+    "revision": "d94c328195735bd4a613178a6bbf6edc"
+  },
+  {
+    "url": "/_next/static/yaALgm2ev7-AeUuP2juzz/_ssgManifest.js",
+    "revision": "abee47769bf307639ace4945f9cfd4ff"
   }
 ];
 
