876dc51183e53bd34e701307f7697b95e9cc9dc7
Fatih Telis <fatih.telis@gmail.com>
2026-01-13 14:22:57 +0300

    remove cache for app-ads.txt

--- next.config.js
+++ next.config.js
@@ -3,13 +3,52 @@ const css = require('@zeit/next-css');
 const fonts = require('next-fonts');
 const offline = require('next-offline');
 
-module.exports = withPlugins([css, fonts, offline], {
-  enableSvg: true,
-  webpack: config => {
-    config.node = {
-      fs: 'empty',
-    };
-
-    return config;
-  },
-});
+module.exports = withPlugins(
+  [
+    css,
+    fonts,
+    [
+      offline,
+      {
+        workboxOpts: {
+          runtimeCaching: [
+            {
+              urlPattern: /^https:\/\/fatihtelis\.com\/app-ads\.txt$/,
+              handler: 'NetworkOnly',
+            },
+          ],
+        },
+      },
+    ],
+  ],
+  {
+    enableSvg: true,
+    webpack: config => {
+      config.node = {
+        fs: 'empty',
+      };
+      return config;
+    },
+    async headers() {
+      return [
+        {
+          source: '/app-ads.txt',
+          headers: [
+            {
+              key: 'Cache-Control',
+              value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
+            },
+            {
+              key: 'Pragma',
+              value: 'no-cache',
+            },
+            {
+              key: 'Expires',
+              value: '0',
+            },
+          ],
+        },
+      ];
+    },
+  }
+);
